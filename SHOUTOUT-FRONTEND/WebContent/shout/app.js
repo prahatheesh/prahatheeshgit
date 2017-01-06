a261dd571aed5c44279ba9a78da144addbf591ab
prahatheesh <admin@172.23.212.169>
2017-01-06 10:37:16 +0530

    COMMIT

--- SHOUTOUT-FRONTEND/WebContent/shout/app.js
+++ SHOUTOUT-FRONTEND/WebContent/shout/app.js
@@ -1,17 +1,18 @@
 var app=angular.module("register",[]);
-app.controller('ctrl',function($scope,$http,$location,$window){
-	var url="http://"+$window.location.host+"/SHOUTOUT-BACKEND";
-	{
+app.controller('ctrl',function($scope,$http,$location,$window)
+		{
+	var url="http://"+$window.location.host+"/SHOUTOUT-BACKEND/";
+	
 	$scope.register=function()
 	{
-	$http.post(url+'register'+$scope.name).success(function(data)
+	$http.post(url+'/register/'+$scope.name).success(function(data)
 	{
 	
 	$log.log(data);
 				
 	});
 	}
-	}
+	
 })
 				
 
