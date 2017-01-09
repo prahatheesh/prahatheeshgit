ca6e26fcd7328485e864559159d348c9e13131cf
prahatheesh <admin@172.23.212.169>
2017-01-09 17:36:40 +0530

    commit

--- SHOUTOUT-BACKEND/src/main/java/com/shoutoutcontroller/Homecontroller.java
+++ SHOUTOUT-BACKEND/src/main/java/com/shoutoutcontroller/Homecontroller.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import com.shoutoutDAO.Daointerface;
 import com.shoutoutmodel.Customer;
 
@@ -19,17 +18,25 @@ public class Homecontroller {
 
 	 public Homecontroller()
 	{
+		 try
+		 {
 	@SuppressWarnings("resource")
 	AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
     context.scan("com.shoutoutconfiguration");
     System.out.println("config");
     context.refresh();
      udao=(Daointerface)context.getBean("udao");
+		 }
+		 catch(Exception e)
+		 {
+			 e.printStackTrace();
+		 }
 	}
-	@RequestMapping(value="/register/{username}",method=RequestMethod.POST)
-			public ResponseEntity<Customer> register(@PathVariable String name){
-		
+	@RequestMapping(value="/register/{username}",method=RequestMethod.POST,headers="Accept=application/json")
+			public ResponseEntity<Customer> register(@PathVariable String username){
+		System.out.println("insie register");
 		Customer custom=new Customer();
+		custom.setUsername(username);
 		udao.insert(custom);
 		
 		return new ResponseEntity<Customer>(HttpStatus.OK);
